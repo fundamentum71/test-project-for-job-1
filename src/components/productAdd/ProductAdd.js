7800d862fc62dc2bdb935ce92047fe3def447d5f
Aleksandr <it.kucherov@gmail.com>
2022-08-05 11:45:32 +0300

    added variables

--- src/components/productAdd/ProductAdd.js
+++ src/components/productAdd/ProductAdd.js
@@ -1,5 +1,32 @@
 import './productAdd.scss';
 
-const ProductAdd = () => {};
+const ProductAdd = () => {
+	return (
+		<form action="#" className="productAdd">
+			<label htmlFor="#productTitle">Наименование товара</label>
+			<input
+				id="productTitle"
+				name=""
+				type="text"
+				placeholder="Введите наименование товара"
+			/>
+
+			<label htmlFor="#productDescr">Описание товара</label>
+			<input
+				id="productDescr"
+				type="text"
+				placeholder="Введите описание товара"
+			/>
+
+			<label htmlFor="#productURL">Ссылка на изображение товара</label>
+			<input id="productURL" type="text" placeholder="Введите ссылку" />
+
+			<label htmlFor="#productPrice">Цена товара</label>
+			<input id="productPrice" type="number" placeholder="Введите цену" />
+
+			<button type="submit">Добавить товар</button>
+		</form>
+	);
+};
 
 export default ProductAdd;
