9c0d602d7d46e8371349afe47927b94d47b4cb0c
Aleksandr <it.kucherov@gmail.com>
2022-08-10 10:21:42 +0300

    img normal view

--- src/components/productListItem/ProductListItem.js
+++ src/components/productListItem/ProductListItem.js
@@ -2,11 +2,11 @@ import './productListItem.scss';
 import trash from '../../resources/img/trash.svg';
 import example from '../../resources/img/example.png';
 
-const ProductListItem = ({ title, desc, ref, price, onDelete }) => {
+const ProductListItem = ({ title, desc, link, price, onDelete }) => {
 	return (
 		<li className="product__list-item">
 			<div className="item__img">
-				<img src={example} alt="product" />
+				<img src={link} alt="product" />
 			</div>
 			<div className="item__wrapper">
 				<h2 className="item__title">{title}</h2>
