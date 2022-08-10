49db95a3ae4c9fe212ecb4379a8cfdb3165c5c55
Aleksandr <it.kucherov@gmail.com>
2022-08-10 15:21:42 +0300

    classes in function

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,82 +1,88 @@
-import { Component } from 'react';
+import { useState, useEffect, useSyncExternalStore } from 'react';
 import ProductSort from '../productSort/ProductSort';
 import ProductAdd from '../productAdd/ProductAdd';
 import ProductList from '../productList/ProductList';
 
-class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: [],
-		};
-		this.maxId = 0;
-	}
+const App = () => {
+	//constructor(props) {
+	//	super(props);
+	//	this.state = {
+	//		data: [],
+	//	};
+	//	this.maxId = 0;
+	//}
 
-	deleteItem = (id) => {
-		this.setState(({ data }) => {
-			return {
-				data: data.filter((item) => item.id !== id),
-			};
-		});
+	const [data, setData] = useState([]);
+	let maxId = 0;
+
+	const deleteItem = (id) => {
+		setData(data.filter((item) => item.id !== id));
 	};
 
-	addItem = (title, desc, link, price) => {
+	const addItem = (title, desc, link, price) => {
 		const newItem = {
 			title,
 			desc,
 			link,
 			price,
-			id: this.maxId++,
+			id: ++maxId,
 		};
-		this.setState(({ data }) => {
-			const newArr = [...data, newItem];
-			return {
-				data: newArr,
-			};
-		});
+
+		const newArr = [...data, newItem];
+		return setData(newArr);
 	};
 
-	onSaveLocal = () => {
-		localStorage.setItem('saveProduct', JSON.stringify(this.state.data));
-		localStorage.setItem('idProduct', JSON.stringify(this.maxId));
-	};
+	//const onSaveLocal = () => {
+	//	localStorage.setItem('saveProduct', JSON.stringify(data));
+	//	localStorage.setItem('idProduct', JSON.stringify(maxId));
+	//};
 
-	onGetLocal = () => {
-		this.setState({ data: JSON.parse(localStorage.getItem('saveProduct')) });
-		this.maxId = JSON.parse(localStorage.getItem('idProduct'));
-	};
+	//const onGetLocal = () => {
+	//	setData(JSON.parse(localStorage.getItem('saveProduct')));
+	//	maxId = JSON.parse(localStorage.getItem('idProduct'));
+	//};
 
-	componentDidMount() {
-		if (
-			localStorage.getItem('saveProduct') !== null &&
-			localStorage.getItem('idProduct') !== null
-		) {
-			this.onGetLocal();
-		}
-	}
-	componentDidUpdate() {
-		this.onSaveLocal();
-	}
+	useEffect(() => {
+		const raw = localStorage.getItem('saveProduct') || [];
+		setData(JSON.parse(raw));
+		//maxId = JSON.parse(localStorage.getItem('idProduct'));
+	}, []);
 
-	render() {
-		const { data } = this.state;
-		return (
-			<div className="App">
-				<header>
-					<h1>Добавление товара</h1>
-					<ProductSort />
-				</header>
-				<main className="main__container">
-					<div>
-						<ProductAdd onAdd={this.addItem} />
-					</div>
-					<div>
-						<ProductList data={data} onDelete={this.deleteItem} />
-					</div>
-				</main>
-			</div>
-		);
-	}
-}
+	useEffect(() => {
+		localStorage.setItem('saveProduct', JSON.stringify(data));
+		//localStorage.setItem('idProduct', JSON.stringify(maxId));
+		console.log('init');
+	}, [data]);
+
+	//componentDidMount = () => {
+	//	if (
+	//		localStorage.getItem('saveProduct') !== null &&
+	//		localStorage.getItem('idProduct') !== null
+	//	) {
+	//		this.onGetLocal();
+	//	}
+	//};
+	//componentDidUpdate = () => {
+	//	onSaveLocal();
+	//};
+
+	//const { data } = this.state;
+	return (
+		<div className="App">
+			<header>
+				<h1>Добавление товара</h1>
+				<ProductSort />
+			</header>
+			<main className="main__container">
+				<div>
+					<ProductAdd onAdd={addItem} />
+				</div>
+				<div>
+					<ProductList data={data} onDelete={deleteItem} />
+				</div>
+			</main>
+		</div>
+	);
+};
 
 export default App;
--- src/components/productAdd/ProductAdd.js
+++ src/components/productAdd/ProductAdd.js
@@ -1,100 +1,102 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import './productAdd.scss';
 
-class ProductAdd extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			title: '',
-			desc: '',
-			link: '',
-			price: '',
-		};
-	}
+const ProductAdd = (props) => {
+	//constructor(props) {
+	//	super(props);
+	//	this.state = {
+	//		title: '',
+	//		desc: '',
+	//		link: '',
+	//		price: '',
+	//	};
+	//}
 
-	onValueChange = (e) => {
-		this.setState({
-			[e.target.name]: e.target.value,
-		});
+	const [title, setTitle] = useState('');
+	const [desc, setDesc] = useState('');
+	const [link, setLink] = useState('');
+	const [price, setPrice] = useState('');
+
+	const onValueChange = (e) => {
+		switch (e.target.name) {
+			case 'title':
+				setTitle(e.target.value);
+				break;
+			case 'desc':
+				setDesc(e.target.value);
+				break;
+			case 'link':
+				setLink(e.target.value);
+				break;
+			case 'price':
+				setPrice(e.target.value);
+				break;
+		}
 	};
 
-	onSubmit = (e) => {
+	const onSubmit = (e) => {
 		e.preventDefault();
 
-		if (
-			this.state.title.length &&
-			this.state.desc.length &&
-			this.state.link.length &&
-			this.state.price.length
-		) {
-			this.props.onAdd(
-				this.state.title,
-				this.state.desc,
-				this.state.link,
-				this.state.price
-			);
+		if (title.length && desc.length && link.length && price.length) {
+			props.onAdd(title, desc, link, price);
 
-			this.setState({
-				title: '',
-				desc: '',
-				link: '',
-				price: '',
-			});
+			setTitle('');
+			setDesc('');
+			setLink('');
+			setPrice('');
 		} else {
 			alert('Заполните данные');
 		}
 	};
 
-	render() {
-		const { title, desc, link, price } = this.state;
-		return (
-			<div className="wrapper-productAdd">
-				<form action="#" className="productAdd" onSubmit={this.onSubmit}>
-					<label htmlFor="#productTitle">Наименование товара</label>
-					<input
-						id="productTitle"
-						name="title"
-						value={title}
-						onChange={this.onValueChange}
-						type="text"
-						placeholder="Введите наименование товара"
-					/>
+	//const { title, desc, link, price } = this.state;
+	return (
+		<div className="wrapper-productAdd">
+			<form action="#" className="productAdd" onSubmit={onSubmit}>
+				<label htmlFor="#productTitle">Наименование товара</label>
+				<input
+					id="productTitle"
+					name="title"
+					value={title}
+					onChange={onValueChange}
+					type="text"
+					placeholder="Введите наименование товара"
+				/>
 
-					<label htmlFor="#productDescr">Описание товара</label>
-					<textarea
-						name="desc"
-						value={desc}
-						onChange={this.onValueChange}
-						id="productDescr"
-						type="text"
-						placeholder="Введите описание товара"
-					></textarea>
+				<label htmlFor="#productDescr">Описание товара</label>
+				<textarea
+					name="desc"
+					value={desc}
+					onChange={onValueChange}
+					id="productDescr"
+					type="text"
+					placeholder="Введите описание товара"
+				></textarea>
 
-					<label htmlFor="#productURL">Ссылка на изображение товара</label>
-					<input
-						id="productURL"
-						type="text"
-						placeholder="Введите ссылку"
-						name="link"
-						value={link}
-						onChange={this.onValueChange}
-					/>
+				<label htmlFor="#productURL">Ссылка на изображение товара</label>
+				<input
+					id="productURL"
+					type="text"
+					placeholder="Введите ссылку"
+					name="link"
+					value={link}
+					onChange={onValueChange}
+				/>
 
-					<label htmlFor="#productPrice">Цена товара</label>
-					<input
-						id="productPrice"
-						type="number"
-						placeholder="Введите цену"
-						name="price"
-						value={price}
-						onChange={this.onValueChange}
-					/>
+				<label htmlFor="#productPrice">Цена товара</label>
+				<input
+					id="productPrice"
+					type="number"
+					placeholder="Введите цену"
+					name="price"
+					value={price}
+					onChange={onValueChange}
+				/>
 
-					<button type="submit">Добавить товар</button>
-				</form>
-			</div>
-		);
-	}
-}
+				<button type="submit">Добавить товар</button>
+			</form>
+		</div>
+	);
+};
 
 export default ProductAdd;
