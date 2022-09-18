1ce7dffb82564490ff478eddfeb08336efe19a3f
Aleksandr <it.kucherov@gmail.com>
2022-09-18 16:27:21 +0300

    сделал сортировку

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,15 @@
 import ProductSort from '../productSort/ProductSort';
 import ProductAdd from '../productAdd/ProductAdd';
 import ProductList from '../productList/ProductList';
+import { useAppSelector } from '../../redux/hook';
 
 const App: React.FC = () => {
+	const sort = useAppSelector((state) => state.filter.sort);
 	return (
 		<div className="App">
 			<header className="header__container">
 				<h1>Добавление товара</h1>
-				<ProductSort />
+				<ProductSort value={sort} />
 			</header>
 			<main className="main__container">
 				<div>
--- src/components/productList/ProductList.tsx
+++ src/components/productList/ProductList.tsx
@@ -5,6 +5,27 @@ import './productList.scss';
 
 const ProductList: React.FC = () => {
 	const cartList = useAppSelector((state) => state.cart.list);
+	const { sortProperty } = useAppSelector((state) => state.filter.sort);
+
+	let nowCartList;
+
+	switch (sortProperty) {
+		case 'default': {
+			nowCartList = cartList;
+			break;
+		}
+		case 'price': {
+			nowCartList = cartList.slice().sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+			break;
+		}
+		case '-price': {
+			nowCartList = cartList.slice().sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
+			break;
+		}
+
+		default:
+			break;
+	}
 
 	const isMounted = React.useRef(false);
 	React.useEffect(() => {
@@ -17,7 +38,7 @@ const ProductList: React.FC = () => {
 
 	return (
 		<ul className="product__list">
-			{cartList.map((item) => (
+			{nowCartList?.map((item) => (
 				<ProductListItem key={item.id} {...item} />
 			))}
 		</ul>
